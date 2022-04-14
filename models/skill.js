da8d0861e1da581e3e0d5f7d8f5aa8b1433446af
jhuang15 <jchuang1020@gmail.com>
2022-04-14 13:58:32 -0400

    Update models skill

--- models/skill.js
+++ models/skill.js
@@ -12,8 +12,30 @@ const skills= [
 
 module.exports = {
   getAll,
-  getOne
+  getOne,
+  create,
+  deleteOne,
+  update
 };
+
+function update(id, skill) {
+  id = parseInt(id);
+  const updatedSkill = skills.find((skill) => skill.id === id);
+  Object.assign(updatedSkill, skill);
+}
+
+function deleteOne(id) {
+  id = parseInt(id);
+  const idx = skills.findIndex((skill) => skill.id === id);
+  skills.splice(idx, 1);
+}
+
+function create(skill) {
+  skill.id = Date.now() % 1000000;
+  skill.done = false;
+  skills.push(skill);
+}
+
 function getAll() {
   return skills;
 }
