2a11ca200bf752616057606ddd459f57b7aa43e5
jhuang15 <jchuang1020@gmail.com>
2022-04-14 13:55:35 -0400

    Update controllers skills

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,20 +1,47 @@
 // Should name the model in uppercase and singular
 const Skill = require('../models/skill');
-
+const { render } = require('../server');
 
 module.exports = {
   index,
-  show
+  show,
+  new: newSkill,
+  create,
+  delete: deleteSkill,
+  edit,
+  update
 };
 
-function index(req, res) {
-  res.render('skills/index', {
-    skills: Skill.getAll()
-  });
+function update(req, res) {
+  req.body.done = !!req.body.done;
+  Todo.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`);
+}
+
+function edit(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/edit', { skill });
+}
+
+function deleteSkill(req, res) {
+  Todo.deleteOne(req.params.id);
+  res.redirect('/skills'); 
+}
+
+function create(req, res) {
+  Skill.create(req.body);
+  res.redirect('/skills')
+}
+function newSkill(req, res) {
+  res.render('skills/new');
 }
 
 function show(req, res) {
-  res.render('skills/show', {
-    skill: Skill.getOne(req.params.id),
-  });
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/show', { skill });
+}
+
+function index(req, res) {
+  const skills = Skill.getAll();
+  res.render('skills/index', { skills })
 }
